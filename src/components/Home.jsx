9e28e28f59e775c916efa3fa0cceb06d4a98641d
Mario Rojas <135486752+MarioDevelop3r@users.noreply.github.com>
2024-06-01 23:04:51 -0400

    Actualizar contenido y estilos del componente Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,6 @@
 import './home.css';  // Archivo CSS para estilos personalizados
 
-const Logo = 
-  "https://i.imgur.com/NVGqkb1.png"
-
+const Logo = "https://i.imgur.com/NVGqkb1.png";
 
 const Home = () => {
   return (
@@ -14,9 +12,13 @@ const Home = () => {
         <div className="texto-banner">
           <h1>CODEBEARD</h1>
           <h2>Nos especializamos en desarrollar software de calidad excepcional.</h2>
-          
-          <p>En CODEBEARD, nos enfocamos en crear productos web seguros y escalables, garantizando soluciones digitales innovadoras que crecen con tu negocio. Creamos plataformas robustas que resisten amenazas digitales y mantienen un rendimiento óptimo en alta demanda, trabajando estrechamente con nuestros clientes para ofrecer soluciones personalizadas que impulsan su éxito en línea. Ofrecemos productos que superan las exigencias de seguridad y escalabilidad, brindando la confianza necesaria para crecer en el entorno digital actual. Contáctanos para hablar sobre tu próximo proyecto digital..</p>
-          <a href="#portafolio" className="cta-button">Vamos</a>
+          <p>
+            En CODEBEARD, nos enfocamos en crear productos web seguros y escalables, garantizando soluciones digitales innovadoras que crecen con tu negocio. Creamos plataformas robustas que resisten amenazas digitales y mantienen un rendimiento óptimo en alta demanda, trabajando estrechamente con nuestros clientes para ofrecer soluciones personalizadas que impulsan su éxito en línea. Ofrecemos productos que superan las exigencias de seguridad y escalabilidad, brindando la confianza necesaria para crecer en el entorno digital actual. Contáctanos para hablar sobre tu próximo proyecto digital.
+          </p>
+          <a href="#portafolio" className="cta-button">
+            Vamos
+            <div className="overlay"></div>
+          </a>
         </div>
       </div>
     </section>
@@ -24,6 +26,7 @@ const Home = () => {
 };
 
 export default Home;
+
 
 
 
