6c9eaaba2e201704e835ffca95d64c96563a6089
Mario Rojas <135486752+MarioDevelop3r@users.noreply.github.com>
2024-08-02 23:28:16 -0400

    feat: Actualizar componente Portfolio con imágenes aleatorias y enlaces

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,40 +2,41 @@ import "../components/portfolio.css";
 
 const Portfolio = () => {
     const randomImages = [
-        { 
-            url: "https://i.imgur.com/LPFx4mo.png", 
-            link: "https://twitercard.netlify.app/", 
-            title: "Twitwer Cards", 
-            description: "Diseño de tarjetas de Twitter" 
+        {
+            url: "https://i.imgur.com/LPFx4mo.png",
+            link: "https://twitercard.netlify.app/",
+            title: "Twitwer Cards",
+            description: "Diseño de tarjetas de Twitter"
         },
-        { 
-            url: "https://i.imgur.com/8iShEQm.png", 
-            link: "https://rick-morty-app-api.netlify.app/", 
-            title: "Rick & Morty API", 
-            description: "Consumo de API"},
-        { 
-            url: "https://picsum.photos/300?random=3", 
-            link: "https://example.com/project3", 
-            title: "Diseño Creativo 3", 
-            description: "Fotografía 3" 
+        {
+            url: "https://i.imgur.com/8iShEQm.png",
+            link: "https://rick-morty-app-api.netlify.app/",
+            title: "Rick & Morty API",
+            description: "Consumo de API"
         },
-        { 
-            url: "https://picsum.photos/300?random=4", 
-            link: "https://example.com/project4", 
-            title: "Diseño Creativo 4", 
-            description: "Fotografía 4" 
+        {
+            url: "https://picsum.photos/300?random=3",
+            link: "https://deploy-preview-1--sprightly-naiad-41d974.netlify.app/",
+            title: "Encriptador de texto - Alura Challenges ONE",
+            description: "aplicación web que permite encriptar y desencriptar textos y funciones adicionales"
         },
-        { 
-            url: "https://picsum.photos/300?random=5", 
-            link: "https://example.com/project5", 
-            title: "Diseño Creativo 5", 
-            description: "Fotografía 5" 
+        {
+            url: "https://picsum.photos/300?random=4",
+            link: "https://example.com/project4",
+            title: "Diseño Creativo 4",
+            description: "Fotografía 4"
         },
-        { 
-            url: "https://picsum.photos/300?random=6", 
-            link: "https://example.com/project6", 
-            title: "Diseño Creativo 6", 
-            description: "Fotografía 6" 
+        {
+            url: "https://picsum.photos/300?random=5",
+            link: "https://example.com/project5",
+            title: "Diseño Creativo 5",
+            description: "Fotografía 5"
+        },
+        {
+            url: "https://picsum.photos/300?random=6",
+            link: "https://example.com/project6",
+            title: "Diseño Creativo 6",
+            description: "Fotografía 6"
         }
     ];
 
